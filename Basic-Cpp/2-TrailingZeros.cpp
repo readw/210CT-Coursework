f7df6265faedb80678bb4666061336aa1369a20c
William Read <readw@users.noreply.github.com>
2016-11-19 15:35:33 +0000

    Add files via upload

---
+++ Basic-Cpp/2-TrailingZeros.cpp
@@ -0,0 +1,31 @@
+/*
+Count the number of trailing 0s (number of 0s at the end of the number) in a
+factorial number.
+
+Example: Input: 5, Output: 1; Input: 10, Output: 2.
+*/
+
+/* #include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int Trailing(int number){
+    int zeros = 0;
+    
+    for (int i = 1; i != number; i++){
+        zeros += number/pow(5,i);
+    }
+    
+    return zeros;
+}
+
+int main() {
+    int input;
+    cout << "Enter a number: ";
+    cin >> input;
+    cout << "Output: " << Trailing(input) << endl;
+    
+    return 0;
+}
+*/
