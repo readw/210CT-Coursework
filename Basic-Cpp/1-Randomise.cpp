9cc2e97c5bff9fd5ee0c23d065fd0a992e1d860a
William Read <readw@users.noreply.github.com>
2016-11-19 15:50:22 +0000

    Update 1-Randomise.cpp

--- Basic-Cpp/1-Randomise.cpp
+++ Basic-Cpp/1-Randomise.cpp
@@ -1,29 +1,38 @@
+/* 
+Week 1 - 1) Write a function that randomly shuffles the array of integers and
+            explain the rationale behind its implementation.
+*/
+
 #include <iostream>
 #include <cstdlib>
 #include <vector>
 using namespace std;
 
 vector<int> Randomise(vector<int> &data) {
-    int i = 0;
-    int length = data.size();
-    int randomI = 0;
+    /* Function to randomise the order of a vector. */
+    int index = 0; /* Default index to first item */
+    int length = data.size(); /* Get length of vector. */
+    int randomIndex = 0; /* Set default randomIndex as 0 */
     vector<int> randomList;
-    while (i++ < length){
-        randomI = (rand()%(data.size()));
-        randomList.insert(randomList.begin(),data[randomI]);
-        data.erase(data.begin()+randomI);
+    while (index++ < length){
+        randomIndex = (rand()%(data.size())); /* Generate a random value of an index in the array. */
+        randomList.insert(randomList.begin(),data[randomIndex]); /* Insert in new vector randomList particular vector. */
+        data.erase(data.begin()+randomIndex); /* Erase the item that has been inserted so it isn't picked again. */
     }
     
     return randomList;
 }
 
 int main() {
+    /* Predefine creation of vector. */
     int value, number;
     vector<int> data;
+    
     
     cout << "Enter length of list: ";
     cin >> value;
     
+    /* Input numbers that are appended to a vector. */
     for (int i = 0; i < value; i++) {
         cout << "Enter a number: ";
         cin >> number;
