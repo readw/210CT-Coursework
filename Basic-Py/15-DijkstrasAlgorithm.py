767577c29a45664900d5193289b109e6c8a59b4e
William Read <readw@users.noreply.github.com>
2016-11-26 16:06:51 +0000

    Uploaded completed 15-DjikstrasAlgorithm.py
    Calculates the shortest path from a created weighted graph.

--- Basic-Py/15-DijkstrasAlgorithm.py
+++ Basic-Py/15-DijkstrasAlgorithm.py
@@ -1,6 +1,7 @@
 # Week 8 - 15) Implement Dijkstras algorithm for a weighted graph data structure
 #              (you have to update your previous data structure so that it can
 #              deal with weights).
+
 class Weighted:
 
     def __init__(self):
@@ -23,10 +24,68 @@ class Weighted:
     
     def displayGraph(self):
         return self.weighted
-    
+
 
 def Dijkstras(graph, start, end):
-    pass
+    '''Performs Dijkstras Algorithm to traverse the graph from start to
+end over a weighted graph.'''
+    # Assign current
+    current = start
+    visited = []
+    # Set previously visited nodes 
+    previous, tentativeWeights = {}, {}
+
+    # Assign all tentative weights for all nodes to infinity.
+    for node in graph:
+        tentativeWeights[node] = float("inf")
+        previous[node] = None
+        
+    # Default start weight as 0.
+    tentativeWeights[start] = 0
+    
+    # Until the current value matches the end result.
+    while current != end:
+        # Define neighbours
+        neighbours = graph[current]
+        for edge, length in neighbours.items():
+            # If distance less than what is currently set.
+            if tentativeWeights[current]+length < tentativeWeights[edge]:
+                # Assign edge to the smaller distance.
+                tentativeWeights[edge] = tentativeWeights[current]+length
+                previous[edge] = current
+        # Append current value to the visited path array.
+        visited.append(current)
+
+        # Set minimum value to infinity.
+        minimum = float("inf")
+        for node in graph:
+            # Check to see if the value isn't in visited and that the weight
+            # is less than that of the minimum path.
+            if (node not in visited) and (tentativeWeights[node] < minimum):
+                # Assign the current value to the new node.
+                current = node
+                # Set the minimum value to the new weight.
+                minimum = tentativeWeights[node]
+
+    visited.append(end)
+    
+    # Generate a path from collected data.
+    path = shortestPath(graph, previous, end)
+    # Return formatted output
+    return "Distance: "+str(tentativeWeights[end])+"\nPath: "+str(path[::-1])
+    
+def shortestPath(graph, previous, end):
+    # Create array to store the result.
+    result = []
+    # Until it reaches parent node.
+    while previous[end] != None:
+        # Add end connection to array.
+        result.append(end)
+        # Set end to it's closest neighbour.
+        end = previous[end]
+    # Append the parent to the reuslt and return the shortest path.
+    result.append(end)
+    return result
 
 if __name__ == "__main__":
     '''
@@ -36,9 +95,9 @@ if __name__ == "__main__":
 
     A -(2)- B -(1)- C -(1)- G -(2)- I
     |     / |               |
-  (1)  (3) (1)      J      (4)
+  (1)  (3) (1)      J      (3)
     | /     |      (1)      |
-    D       E -(2)- F       H
+    D       E -(2)- F -(2)- H
     '''
     
     graph = Weighted()
@@ -61,10 +120,14 @@ if __name__ == "__main__":
     graph.addEdge("D","B",3)
     graph.addEdge("E","F",2)
     graph.addEdge("F","J",1)
-    graph.addEdge("G","H",4)
+    graph.addEdge("F","H",2)
+    graph.addEdge("G","H",3)
     graph.addEdge("G","I",2)
 
     weightedGraph = graph.displayGraph()
 
-    print(weightedGraph)
-    print(Dijkstras(graph, 'A', 'J'))
+    # Uncomment to view weighted graph that has been generated.
+    #print(weightedGraph)
+
+    # Calculate the shortest path using Dijkstras Algorithm.
+    print(Dijkstras(weightedGraph, 'A', 'J'))
